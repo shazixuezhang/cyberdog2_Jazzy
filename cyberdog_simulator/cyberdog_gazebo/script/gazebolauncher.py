84261eeaf861e59ad1e9750b1ca6a7dbe248ac7e
shazixuezhang <lqxuezhang@qq.com>
2026-07-09 21:26:11 +0800

    修改Rviz2启动后的默认配置和Gazebo关闭后的清理脚本

--- cyberdog_simulator/cyberdog_gazebo/script/gazebolauncher.py
+++ cyberdog_simulator/cyberdog_gazebo/script/gazebolauncher.py
@@ -1,36 +1,34 @@
-#!/usr/bin/env python
-# Copyright (c) 2023-2023 Beijing Xiaomi Mobile Software Co., Ltd. All rights reserved.
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#      http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+#!/usr/bin/env python3
 import sys, signal, subprocess, time
 
+timeout_before_kill = 0.8
+timeout_after_kill = 1.0
 
-timeout_before_kill = 1.0  # [s]
-timeout_after_kill = 1.0  # [s]
-
-
-def signal_handler(sig, frame):
-    print('killing gazebo')
-    time.sleep(timeout_before_kill)
-    subprocess.call("killall -q gzclient & killall -q gzserver", shell=True)
+def clean_gazebo_all():
+    print("[Clean] 优雅终止gazebo与ros_gz桥接进程")
+    # 优雅关闭
+    subprocess.run("pkill -f 'gz sim'", shell=True, capture_output=True)
     time.sleep(timeout_after_kill)
-    subprocess.call("killall -9 -q gzclient & killall -9 -q gzserver", shell=True)
+    # 强制杀残留
+    subprocess.run("pkill -9 -f 'gz sim'", shell=True, capture_output=True)
+    print("[Clean] 所有gz仿真进程清理完成")
+
+def sig_handler(sig, frame):
+    print("\n收到Ctrl+C，开始清理仿真进程...")
+    time.sleep(timeout_before_kill)
+    clean_gazebo_all()
     sys.exit(0)
 
-
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
-    cmd = ' '.join(sys.argv[1:])
-    print(cmd)
-    subprocess.call(cmd, shell=True)
+    # 监听多种终止信号
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+    signal.signal(signal.SIGHUP, sig_handler)
+
+    run_cmd = ' '.join(sys.argv[1:])
+    print(f"启动指令：{run_cmd}")
+    # 阻塞运行仿真launch
+    ret_code = subprocess.call(run_cmd, shell=True)
+    # 仿真正常退出也自动清理残留
+    clean_gazebo_all()
+    sys.exit(ret_code)
